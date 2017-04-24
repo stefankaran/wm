b731a85c0b3603329d99bd67695d611a1a9d90a5
Stefan Karan <stefankaran88@gmail.com>
2017-04-24 23:10:19 +0200

    i

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,9 @@
-$(document).ready(function(){
+$.fn.dataTable.ext.pager.numbers_length = 7;
+$(document).ready(function() {
 
-});
+    $('#table').DataTable( {
+        "ajax": "data/info.txt",
+         "dom": '<"table-header"f><"table-main"t><"table-footer"lp><"clear">'
+    } );
+    
+} );
